f1a785947ab95153e1e030add4411cce48bb7378
Tyler Jachetta <me@tylerjachetta.net>
2018-12-22 09:13:39 -0800

    some updates in the library

--- hb_lib/testing.py
+++ hb_lib/testing.py
@@ -11,5 +11,24 @@ have before? Who knows?
 """
 
 import collections
+import unittest
 
-CaseParameters = collections.namedtuple('CaseParameters', ['input', 'result'])
+CaseParameters = collections.namedtuple('CaseParameters', ['input_data', 'result'])
+
+
+class ParameterizedCase(unittest.TestCase):
+
+    def __init__(self, case_params, method='runTest', run_function=None):
+        self._case_params = case_params
+        self._run_function = run_function
+
+        super(ParameterizedCase, self).__init__(method)
+
+    def runTest(self):
+
+        if self._run_function is None:
+            msg = "this {} has not been set up properly".format(self.__class__.__name__)
+            raise NotImplementedError(msg)
+
+            res = self._run_function(self._case_params.input_data)
+            self.assertEqual(res, self._case_params.result)
