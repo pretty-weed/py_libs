b9ff0e448c494bf6b569bf32088073e217796f81
Dandelion Good <dandeliongood@proton.me>
2025-09-07 12:52:28 -0600

    More work

--- src/dandy_lib/datatypes/numeric.py
+++ src/dandy_lib/datatypes/numeric.py
@@ -1,4 +1,22 @@
 
+
+
+def bounded_int_factory(min_val, max_val):
+    foo = 3
+    here_min, here_max = min_val, max_val
+    class BoundedInt(int):
+        bar = foo
+        min_val = here_min
+        max_val = here_max
+
+        def __new__(cls, in_val=0):
+            if not cls.min_val <= in_val <= cls.max_val:
+                raise ValueError(f"Value {in_val} is not in range {cls.min_val}, {cls.max_val}")
+            return super().__new__(cls)
+
+    return BoundedInt
+
+
 class NonNegInt(int):
     @classmethod
     def __new__(cls, val):
