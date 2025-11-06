94a32fbaf64dfcc194f18f99f765345e343005db
Dandelion Good <dandeliongood@proton.me>
2025-11-05 20:23:10 -0700

    linting: linted file

--- src/dandy_lib/cli/parser_actions.py
+++ src/dandy_lib/cli/parser_actions.py
@@ -4,7 +4,7 @@ from typing import Any
 
 
 class ForceableFailingAction(Action):
-    EXCEPTIONS_TO_CATCH = (NotImplementedError, )
+    EXCEPTIONS_TO_CATCH = (NotImplementedError,)
     FORCE_DEST = "force"
     FORCE_FLAG = "--force"
 
@@ -13,26 +13,41 @@ class ForceableFailingAction(Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         if self.FORCE_FLAG not in self._force_parsers:
             force_parser = ArgumentParser()
-            force_parser.add_argument(self.FORCE_FLAG, dest=self.FORCE_DEST, action="store_true")
+            force_parser.add_argument(
+                self.FORCE_FLAG, dest=self.FORCE_DEST, action="store_true"
+            )
             self.__class__._force_parsers[self.FORCE_FLAG] = force_parser
 
-        parsed, _unparsed = self._force_parsers[self.FORCE_FLAG].parse_known_args()
+        parsed, _unparsed = self._force_parsers[
+            self.FORCE_FLAG
+        ].parse_known_args()
         self.force = getattr(parsed, self.FORCE_DEST)
         super().__init__(option_strings, dest, nargs=nargs, **kwargs)
 
     def _get_val(self, parser, namespace, values, option_string=None) -> Any:
         raise NotImplementedError()
 
-    def _do_check(self, parser, namespace, values, result, option_string=None) -> None:
+    def _do_check(
+        self, parser, namespace, values, result, option_string=None
+    ) -> None:
         raise NotImplementedError()
 
     def __call__(self, parser, namespace, values, option_string=None):
 
-        print('%r %r %r' % (namespace, values, option_string))
-        results = [self._get_val(parser, namespace, value, option_string) for value in values]
+        print("%r %r %r" % (namespace, values, option_string))
+        results = [
+            self._get_val(parser, namespace, value, option_string)
+            for value in values
+        ]
         for result, value in zip(results, values):
             try:
-                self._do_check(parser, namespace, value, result, option_string=option_string)
+                self._do_check(
+                    parser,
+                    namespace,
+                    value,
+                    result,
+                    option_string=option_string,
+                )
             except self.EXCEPTIONS_TO_CATCH as exc:
                 if not self.force:
                     raise exc
