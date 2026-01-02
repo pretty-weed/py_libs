b43ae76cbd901b1250efa2486f4a49c17c943f43
Dandelion Good <dandeliongood@proton.me>
2026-01-01 20:53:33 -0700

    WIP

--- src/dandy_lib/datatypes/numeric.py
+++ src/dandy_lib/datatypes/numeric.py
@@ -1,25 +1,7 @@
-def bounded_int_factory(min_val, max_val):
-    foo = 3
-    here_min, here_max = min_val, max_val
+from typing import Annotated, TypeAlias
 
-    class BoundedInt(int):
-        bar = foo
-        min_val = here_min
-        max_val = here_max
-
-        def __new__(cls, in_val=0):
-            if not cls.min_val <= in_val <= cls.max_val:
-                raise ValueError(
-                    f"Value {in_val} is not in range {cls.min_val}, {cls.max_val}"
-                )
-            return super().__new__(cls)
-
-    return BoundedInt
+from annotated_types import Ge
 
 
-class NonNegInt(int):
-    @classmethod
-    def __new__(cls, val):
-        if val < 0:
-            raise ValueError("Negative values not allowed")
-        super().__new__(val)
+NonNegInt: TypeAlias = Annotated[int, Ge(0)]
+NonNegFloat: TypeAlias = Annotated[float, Ge(0.0)]
--- src/dandy_lib/datatypes/twodee.py
+++ src/dandy_lib/datatypes/twodee.py
@@ -1,19 +1,83 @@
-from dataclasses import dataclass
-from typing import Iterator
+from functools import cached_property
+from typing import Annotated, Any, NamedTuple, TypeAlias
 
-from .numeric import NonNegInt
+from annotated_types import Ge
+from .numeric import NonNegFloat, NonNegInt
 
 
-@dataclass(unsafe_hash=True)
-class Size:
-    width: NonNegInt
-    height: NonNegInt
+class Size(NamedTuple):
+    width: NonNegInt | NonNegFloat
+    height: NonNegInt | NonNegFloat
 
     @classmethod
-    def factory(cls, *in_vals):
+    def factory(cls, *in_vals: NonNegInt | NonNegFloat):
         if len(in_vals) == 1:
             return cls(in_vals[0], in_vals[0])
         return cls(*in_vals)
 
-    def __iter__(self) -> Iterator[int]:
-        yield from [self.width, self.height]
+
+type Number = int | float
+
+
+class Vector(NamedTuple):
+    x: Number
+    y: Number
+
+
+class Coord(Vector):
+    pass
+
+
+class Rect(NamedTuple):
+    position: Coord
+    size: Size
+
+    def __getattr__(self, name) -> Number:
+        """
+        Automagically get child element attributes (e.g. width, x)
+        so frickin lazy
+        """
+        raise_exc: None | AttributeError = None
+        for element in self:
+            try:
+                return getattr(element, name)
+            except AttributeError:
+                pass
+
+        # Gonna raise an exception
+        try:
+            return super().__getattr__(name)  # type: ignore
+        except AttributeError:
+            raise AttributeError(f"{type(self)} has no attribute {name}")
+
+    @cached_property
+    def top(self) -> Number:
+        return self.y
+
+    @cached_property
+    def bottom(self) -> Number:
+        return self.y + self.height
+
+    @cached_property
+    def left(self) -> Number:
+        return self.y
+
+    @cached_property
+    def right(self) -> Number:
+        return self.y + self.width
+
+    @cached_property
+    def top_left(self) -> Coord:
+        return Coord(self.x, self.y)
+
+    @cached_property
+    def bottom_left(self) -> Coord:
+        return Coord(self.bottom, self.right)
+
+    @cached_property
+    def top_right(self) -> Coord:
+        return Coord(self.x, self.right)
+
+    @cached_property
+    def bottom_right(self) -> Coord:
+        return Coord(self.bottom, self.right)
