a91e256bb3a078c68d2c0d9b45f378faf72c1b8e
Dandelion Good <dandeliongood@proton.me>
2026-04-02 09:31:48 -0600

    Some minor improvements

--- src/dandy_lib/testing.py
+++ src/dandy_lib/testing.py
@@ -1,7 +1,7 @@
 """
 testing.py
 
-shared conts and code used by the unnecessary testing framework.
+shared consts and code used by the unnecessary testing framework.
 You know, because I'm a belt and suspenders and jackhammer gal.
 Why is there a jackhammer? maybe I need to be in practice with
 using a jackhammer for something, despite the fact that I never
